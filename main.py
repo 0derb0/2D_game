f6f470db8b65871dfd52d5a26e7dd4413abbe3fb
0derb0 <ni3132887@gmail.com>
2023-07-03 17:15:22 +0300

    перенес глобальные переменные в отдельный файл и разбил спавн роботов на функции

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from random import randint
 from newItem import Item, movingItem, animatedItem, controlItem
 from work_with_json import inJson
+from Robot_spawn import *
+import consts
 
 
 pygame.init()
@@ -20,48 +22,41 @@ pygame.display.set_icon(icon)
 
 # screen size
 screen_size = pygame.display.get_window_size()
-screen_width = screen_size[0]
-screen_height = screen_size[1]
-
-# score and life
-score = 0
-life = 10
+screen_height = screen_size[0]
+screen_width = screen_size[1]
 
 # create font
 font = pygame.font.Font(None, 36)
 menu_font = pygame.font.Font(None, 55)
 
-# recharge
-reloading = False
-cage_count_const = 30
-cage_count = 30
-cd_time_const = 90
-cd_time = 90
+gun_data = consts.Gun_data(
+    reloading=False, cage_count_const=30, cage_count=30,
+    cd_time_const=90, cd_time=90
+)
 
-# run the game
-run = True
-gameplay = False
-pause = False
-game_menu = True
+glob_data = consts.Global_data(
+    height=screen_size[1], width=screen_size[0], fps=100,
+    run=True, gameplay=False, pause=False, game_menu=True,
+    score=0, life=10
+)
 
 # robot group and timers
-current_time = 0
+
 robot_group = pygame.sprite.Group()
-robot1_time = 105
-robot2_time = 125
-standard_robot_speed = screen_width/240
-wave_count = 0
-final_wave = 11
-spawned1 = 0
-spawned2 = 0
-level_boss_alive = True
+
+rb_data = consts.Robot_data(
+    current_time=0, robot1_time=105, robot2_time=125,
+    standard_robot_speed=glob_data.width / 240,
+    wave_count=0, final_wave=11, spawned1=0, spawned2=0,
+    level_boss_alive=True
+)
 
 # items
 
 # create a gun
 gun = controlItem(
     pash='img/gun1.png',
-    x=100, y=screen_height/2,
+    x=100, y=glob_data.height/2,
     width=200, height=200,
     flip=True, velocity=8
 )
@@ -70,8 +65,8 @@ gun_group = pygame.sprite.GroupSingle(gun)
 # create a collide line
 red_line = Item(
     path='img/1.png',
-    x=screen_width/5, y=0,
-    width=5, height=screen_height
+    x=glob_data.width/5, y=0,
+    width=5, height=glob_data.height
 )
 red_group = pygame.sprite.GroupSingle(red_line)
 
@@ -90,19 +85,20 @@ for num in range(1, 5):
     img = pygame.transform.scale(img, (100, 100))
     old_images.append(img)
 
-while run:
-    if gameplay:
+
+while glob_data.run:
+    if glob_data.gameplay:
 
         # wave_count = 1
         key = pygame.key.get_pressed()
 
         # fps, bg-color, texts
-        clock.tick(fps)
+        clock.tick(glob_data.fps)
         screen.fill((200, 162, 200))
-        score_text = font.render(f'Score: {score}', True, (180, 0, 0))
-        life_text = font.render(f'{life} lifes', True, (180, 0, 0))
-        cage_count_text = font.render(f'{cage_count} bullets', True, (180, 0, 0))
-        data = font.render(f'1:{spawned1}, 2:{spawned2}, wave: {wave_count}, ct: {current_time}, rb1: {robot1_time}, rb2: {robot2_time}', True, (180, 0, 0))
+        score_text = font.render(f'Score: {glob_data.score}', True, (180, 0, 0))
+        life_text = font.render(f'{glob_data.life} lifes', True, (180, 0, 0))
+        cage_count_text = font.render(f'{gun_data.cage_count} bullets', True, (180, 0, 0))
+        data = font.render(f'1:{rb_data.spawned1}, 2:{rb_data.spawned2}, wave: {rb_data.wave_count}, rb1: {rb_data.robot1_time}, rb2: {rb_data.robot2_time}, ct: {rb_data.current_time}', True, (180, 0, 0))
 
         # get events
         for e in pygame.event.get():
@@ -110,14 +106,14 @@ while run:
 
             # leave the game
             if e.type == pygame.QUIT:
-                run = False
+                glob_data.run = False
             if key[pygame.K_ESCAPE]:
-                run = False
+                glob_data.run = False
             if key[pygame.K_q]:
-                pause = True
+                glob_data.pause = True
 
             # shooting
-            if key[pygame.K_SPACE] and not reloading and e.type == pygame.KEYDOWN:
+            if key[pygame.K_SPACE] and not gun_data.reloading and e.type == pygame.KEYDOWN:
                 x = gun.rect.x + 110
                 y = gun.rect.y + 35
                 bullet_group.add(movingItem(
@@ -126,143 +122,61 @@ while run:
                     width=30, height=30,
                     speed=50
                 ))
-                cage_count -= 1
-                if cage_count <= 0:
-                    reloading = True
+                gun_data.cage_count -= 1
+                if gun_data.cage_count <= 0:
+                    gun_data.reloading = True
 
         # reloading
-        if (key[pygame.K_r]) or reloading == True:
-            reloading = True
-            cd_time -= 1
-            screen.blit(cage_count_text, (screen_width-200, 140))
-            if cd_time <= 0:
-                cage_count = cage_count_const
-                cd_time = cd_time_const
-                screen.blit(cage_count_text, (screen_width-200, 140))
-                reloading = False
+        if (key[pygame.K_r]) or gun_data.reloading:
+            gun_data.reloading = True
+            gun_data.cd_time -= 1
+            screen.blit(cage_count_text, (glob_data.width-200, 140))
+            if gun_data.cd_time <= 0:
+                gun_data.cage_count = gun_data.cage_count_const
+                gun_data.cd_time = gun_data.cd_time_const
+                screen.blit(cage_count_text, (glob_data.width-200, 140))
+                gun_data.reloading = False
 
         # write a texts
-        screen.blit(score_text, (screen_width-200, 60))
-        screen.blit(life_text, (screen_width-200, 100))
-        screen.blit(cage_count_text, (screen_width-200, 140))
-        screen.blit(data, (20, screen_height-25))
+        screen.blit(score_text, (glob_data.width-200, 60))
+        screen.blit(life_text, (glob_data.width-200, 100))
+        screen.blit(cage_count_text, (glob_data.width-200, 140))
+        screen.blit(data, (20, glob_data.height-25))
 
-        if robot2_time <= 1250:
-            wave_count = 1
-        if robot2_time > 1250 and robot2_time <= 3000:
-            wave_count = 2
-        if 3000 < robot2_time and robot2_time <= 5000:
-            wave_count = 3
-        if 5000 < robot2_time and robot2_time <= 8000:
-            wave_count = 4
-        if 8000 < robot2_time and robot2_time <= 11000:
-            wave_count = 5
-        if 11000 <= robot2_time and robot2_time <= 13000:
-            wave_count = 6
-        if 13000 <= robot2_time and robot2_time <= 15000:
-            wave_count = 7
-        if 15000 <= robot2_time and robot2_time <= 16500:
-            wave_count = 8
-        if 16500 <= robot2_time and robot2_time <= 18000:
-            wave_count = 9
-        if 18000 <= robot2_time and robot2_time <= 19000:
-            wave_count = 10
-        if 19000 <= robot2_time and level_boss_alive:
-            wave_count = 11
+        rb_data.wave_count = Wave_count(
+            rb_time=rb_data.robot2_time, lba=rb_data.level_boss_alive
+        )
 
-
-
-        if current_time >= robot1_time and 0 < wave_count < 4:
-            robot_group.add(animatedItem(
-                    path='img/robots2/robot_2.0.png',
-                    x=screen_width, y=randint(20, screen_height - 30),
-                    width=100, height=100,
-                    speed=standard_robot_speed, anim=images
-                ))
-            spawned1 += 1
-            robot1_time += 105
-        if current_time >= robot2_time and 0 < wave_count < 4:
-            robot_group.add(
-                animatedItem(
-                    path='img/mob/mob1-removebg-preview.png',
-                    x=screen_width, y=randint(20, screen_height - 30),
-                    width=100, height=100,
-                    speed=standard_robot_speed, anim=old_images
-                )
-            )
-            spawned2 += 1
-            robot2_time += 125
-            # 10 robot2 = 1250
-            # 3000 - 24rb2
-            # 40rb2 = 5000
-
-        if current_time >= robot1_time and 3 < wave_count < 8:
-            robot_group.add(animatedItem(
-                    path='img/robots2/robot_2.0.png',
-                    x=screen_width, y=randint(20, screen_height - 30),
-                    width=100, height=100,
-                    speed=standard_robot_speed, anim=images
-                ))
-            spawned1 += 1
-
-            robot1_time += 90
-        if current_time >= robot2_time and 3 < wave_count < 8:
-            robot_group.add(
-                animatedItem(
-                    path='img/mob/mob1-removebg-preview.png',
-                    x=screen_width, y=randint(20, screen_height - 30),
-                    width=100, height=100,
-                    speed=standard_robot_speed, anim=old_images
-                )
-            )
-            spawned2 += 1
-
-            robot2_time += 115
-
-
-        if current_time >= robot1_time and wave_count > 7:
-            robot_group.add(animatedItem(
-                path='img/robots2/robot_2.0.png',
-                x=screen_width, y=randint(20, screen_height - 30),
-                width=100, height=100,
-                speed=standard_robot_speed, anim=images
+        if rb_data.current_time >= rb_data.robot1_time:
+            robot_group.add(new_robot(
+                glob_data.width, glob_data.height,
+                rb_data.standard_robot_speed, images
             ))
-            spawned1 += 1
+            rb_data.robot1_time += rb_time(rb_data.wave_count, 'rb1')
 
-            robot1_time += 80
-
-        if current_time >= robot2_time and wave_count > 7:
-            robot_group.add(
-                animatedItem(
-                    path='img/mob/mob1-removebg-preview.png',
-                    x=screen_width, y=randint(20, screen_height - 30),
-                    width=100, height=100,
-                    speed=standard_robot_speed, anim=old_images
-                )
-            )
-            spawned2 += 1
-
-            robot2_time += 105
-
-        if current_time >= robot1_time and wave_count == final_wave:
-            robot_group.add(animatedItem(
-                path='img/robots2/robot_2.0.png',
-                x=screen_width, y=randint(20, screen_height - 30),
-                width=100, height=100,
-                speed=standard_robot_speed, anim=images
+        if rb_data.current_time >= rb_data.robot2_time:
+            robot_group.add(new_robot(
+                glob_data.width, glob_data.height,
+                rb_data.standard_robot_speed, old_images
             ))
-            robot_group.add(animatedItem(
-                    path='img/mob/mob1-removebg-preview.png',
-                    x=screen_width, y=randint(20, screen_height - 30),
-                    width=100, height=100,
-                    speed=standard_robot_speed, anim=old_images
-                ))
-            robot1_time += 90
+            rb_data.robot2_time += rb_time(rb_data.wave_count, 'rb2')
+
+        if rb_data.current_time >= rb_data.robot1_time \
+                and rb_data.wave_count == rb_data.final_wave:
+            robot_group.add(new_robot(
+                glob_data.width, glob_data.height,
+                rb_data.standard_robot_speed, images
+            ))
+            robot_group.add(new_robot(
+                glob_data.width, glob_data.height,
+                rb_data.standard_robot_speed, old_images
+            ))
+            rb_data.robot1_time += 90
 
         # gun move
         gun.move(pygame.K_w, pygame.K_s)
-        if gun.position_y > screen_height:
-            gun.position_y = screen_height
+        if gun.position_y > glob_data.height:
+            gun.position_y = glob_data.height
         if gun.position_y < 1:
             gun.position_y = 1
 
@@ -282,27 +196,29 @@ while run:
         # collides
         red_collide = pygame.sprite.groupcollide(red_group, robot_group, False, True)
         if red_collide:
-            life -= 1
-            screen.blit(life_text, (screen_width-200, 100))
-            if life <= 0:
-                if score != 0:
-                    result = inJson('results.json').new_value(score)
+            glob_data.life -= 1
+            screen.blit(life_text, (glob_data.width-200, 100))
+            if glob_data.life <= 0:
+                if glob_data.score != 0:
+                    result = inJson('results.json').new_value(glob_data.score)
+                glob_data.gameplay = False
 
         robot_collide = pygame.sprite.groupcollide(bullet_group, robot_group, True, True)
         if robot_collide:
-            score += 1
+            glob_data.score += 1
 
         # update display and mob spawn timer
         pygame.display.update()
-        current_time += 1.25
-    elif not gameplay and not pause and not game_menu:
+        rb_data.current_time += 1.25
+    elif not glob_data.gameplay and not glob_data.pause \
+            and not glob_data.game_menu:
 
         text_color = (0, 0, 0)
-        text_width = screen_width/2-100
-        text_height = screen_height/3
+        text_width = glob_data.width/2-100
+        text_height = glob_data.height/3
 
         lose_label = menu_font.render('Вы проиграли!', False, text_color)
-        score_label = menu_font.render(f'Счет: {score}', False, text_color)
+        score_label = menu_font.render(f'Счет: {glob_data.score}', False, text_color)
         lose_btn = menu_font.render('Заново', False, text_color)
         lose_btn_rect = lose_btn.get_rect(topleft=(text_width, text_height+120))
         exit_btn = menu_font.render('Выйти', False, text_color)
@@ -322,34 +238,33 @@ while run:
         mouse = pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0]:
             if lose_btn_rect.collidepoint(mouse):
-                gameplay = True
+                glob_data.gameplay = True
                 robot_group.empty()
                 bullet_group.empty()
 
-                life = 10
-                cage_count = cage_count_const
-                score = 0
+                glob_data.life = 10
+                gun_data.cage_count = gun_data.cage_count_const
+                glob_data.score = 0
             if exit_btn_rect.collidepoint(mouse):
-                run = False
+                glob_data.run = False
 
         for e in pygame.event.get():
             key = pygame.key.get_pressed()
             if e.type == pygame.QUIT:
-                run = False
+                glob_data.run = False
             if key[pygame.K_ESCAPE]:
-                run = False
+                glob_data.run = False
         pygame.display.update()
-
-    if pause:
-        gameplay = False
+    if glob_data.pause:
+        glob_data.gameplay = False
 
         color_text = (255, 255, 255)
-        text_width = screen_width/2-100
-        text_height = screen_height/3
+        text_width = glob_data.width/2-100
+        text_height = glob_data.height/3
 
         pause_label = menu_font.render('Пауза', False, color_text)
-        score_label = menu_font.render(f'Счет: {score}', False, color_text)
-        life_label = menu_font.render(f'Жизней: {life}', False, color_text)
+        score_label = menu_font.render(f'Счет: {glob_data.score}', False, color_text)
+        life_label = menu_font.render(f'Жизней: {glob_data.life}', False, color_text)
         continue_btn = menu_font.render('Продолжить', False, color_text)
         continue_btn_rect = continue_btn.get_rect(topleft=(text_width, text_height+180))
         exit_btn = menu_font.render('Выйти', False, color_text)
@@ -370,26 +285,26 @@ while run:
         mouse = pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0]:
             if continue_btn_rect.collidepoint(mouse):
-                gameplay = True
-                pause = False
+                glob_data.gameplay = True
+                glob_data.pause = False
             if exit_btn_rect.collidepoint(mouse):
-                run = False
+                glob_data.run = False
 
         for e in pygame.event.get():
             key = pygame.key.get_pressed()
             if e.type == pygame.QUIT:
-                run = False
+                glob_data.run = False
             if key[pygame.K_ESCAPE]:
-                run = False
+                glob_data.run = False
             if key[pygame.K_q]:
-                gameplay = True
-                pause = False
+                glob_data.gameplay = True
+                glob_data.pause = False
         pygame.display.update()
-    if not gameplay and game_menu:
+    if not glob_data.gameplay and glob_data.game_menu:
 
         text_color = (121, 168, 50)
-        text_width = screen_width / 2 - 100
-        text_height = screen_height / 3
+        text_width = glob_data.width / 2 - 100
+        text_height = glob_data.height / 3
 
         game_label = menu_font.render('UwU', False, text_color)
         label = menu_font.render('Меню', False, text_color)
@@ -413,20 +328,20 @@ while run:
         for ev in pygame.event.get():
             key = pygame.key.get_pressed()
             if ev.type == pygame.QUIT:
-                run = False
+                glob_data.run = False
             if key[pygame.K_ESCAPE]:
-                run = False
+                glob_data.run = False
             if key[pygame.K_q]:
-                gameplay = True
-                game_menu = False
+                glob_data.gameplay = True
+                glob_data.game_menu = False
 
         mouse = pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0]:
             if go_btn_rect.collidepoint(mouse):
-                gameplay = True
-                game_menu = False
+                glob_data.gameplay = True
+                glob_data.game_menu = False
             if exit_btn_rect.collidepoint(mouse):
-                run = False
+                glob_data.run = False
 
         pygame.display.update()
 
